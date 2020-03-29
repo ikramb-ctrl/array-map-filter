8d757e950c2c18704a7297ca32d0c639669bef67
ikramb-ctrl <59873336+ikramb-ctrl@users.noreply.github.com>
2020-03-29 18:58:30 +0200

    Update ex1.js

--- filter/ex1.js
+++ filter/ex1.js
@@ -14,6 +14,7 @@ Expected OUTPUT for this sample:
 */
 
 function getPositiveNumbers(numbers) {
+  return getPositiveNumbers.filter((numbers) => word.length === 0);
 }
 
 // If you need, you can always visualize the result with a console.log.
